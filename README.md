f0228d52266f5c8f467f1525a4b09685188e32a2
Bradley A. Thornton <bthornto@thethorntons.net>
2019-08-17 12:25:03 -0700

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# Network Mock
+
+`network_mock` is a simple ssh server that can respond to network device `show` commands. It was authored as a debugging tool used during local development.
+
+## Getting started
+
+The previously retrieved command output needs to be stored in a directory, one per device. The file needs to be named as the exact command.
+
+```
+├── eos101
+│   └── show run.txt
+├── ios101
+│   └── show run.txt
+├── nxos101
+│   └── show run.txt
+└── vyos101
+    └── show config.txt
+```
+
+## Starting the server
+
+Update the server.py to reflect the base port and number of servers.
+The base port is by default 3000.
+
+A key needs to be provided:
+
+```
+python server.py test_rsa.key
+```
+
+## Using with ansible
+
+At the beginning of the play, the ssh server needs to be informed of the name of the network device.  This is used as the folder name from which the command output will be retrieved.
+
+Issue the command in the format `meta: hostname=xxxx`, where xxx is the name of the device for which `show` command output will be retrieved.
+
+```
+tasks:
+- cli_command:
+    command: "meta: hostname={{ inventory_hostname }}"
+```
+
+The ansible invnetory should be updated such that each host uses a unique port on the SSH mock server and the `ansible_host` set to where the server was started.
+
+```
+vars:
+  ansible_port: "{{ 3000 + play_hosts.index(inventory_hostname) }}"
+  ansible_host: localhost
+```
---
+++ server.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+import socket
+import sys
+import threading
+import re
+import concurrent.futures
+import paramiko
+
+BASE_PORT = 3000
+SERVER_COUNT = 10
+CONFIG_DIR = "./configs"
+
+
+class Server(paramiko.ServerInterface):
+    """ paramiko server
+    """
+
+    def __init__(self):
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        return paramiko.OPEN_SUCCEEDED
+
+    def check_auth_password(self, username, password):
+        return paramiko.AUTH_SUCCESSFUL
+
+    def get_allowed_auths(self, username):
+        return "password"
+
+    def check_channel_pty_request(
+        self, channel, term, width, height, pixelwidth, pixelheight, modes
+    ):
+        return True
+
+    def check_channel_shell_request(self, channel):
+        return True
+
+    def check_channel_subsystem_request(self, channel, name):
+        return True
+
+    def check_channel_exec_request(self, channel, command):
+        self.event.set()
+        return True
+
+
+def listener(host_key_path, port):
+    host_key = paramiko.RSAKey(filename=host_key_path)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("", port))
+
+    sock.listen(100)
+    client, _addr = sock.accept()
+
+    transport = paramiko.Transport(client)
+    # ttransport.set_gss_host(socket.getfqdn(""))
+    transport.load_server_moduli()
+    transport.add_server_key(host_key)
+    server = Server()
+    transport.start_server(server=server)
+
+    channel = transport.accept(20)
+    if channel is None:
+        sys.exit(1)
+
+    prompt = "router#"
+    meta = {}
+    fhandle = channel.makefile("rU")
+
+    channel.send("\n{}".format(prompt))
+    while True:
+        line = fhandle.readline().rstrip()
+        print(line)
+        metacmd = re.match(r"meta: (?P<key>.*)=(?P<value>.*)", line)
+        if metacmd:
+            cap = metacmd.groupdict()
+            meta[cap["key"]] = cap["value"]
+        showcmd = re.match(r"show (?P<value>.*)", line)
+        if showcmd:
+            cap = showcmd.groupdict()
+            if cap["value"] == "privilege" and "hostname" not in meta:
+                channel.send("\n15")
+            elif (
+                cap["value"] in ["version", "inventory"]
+                and "hostname" not in meta
+            ):
+                pass
+            elif "hostname" not in meta:
+                channel.send(
+                    '% hostname must be set with a "meta: hostname=xxxx" command'
+                )
+            else:
+                try:
+                    with open(
+                        "{}/{}/{}.txt".format(
+                            CONFIG_DIR, meta["hostname"], line
+                        ),
+                        "r",
+                    ) as fhand:
+                        content = fhand.read()
+                    channel.send(content)
+                    print(content)
+                except FileNotFoundError:
+                    channel.send(
+                        '% command file missing for "{}"'.format(line)
+                    )
+        channel.send("\n{}".format(prompt))
+
+
+def _spawn(host_key_path, port):
+    while True:
+        try:
+            listener(host_key_path, port)
+        except Exception:  # pylint: disable=W0703
+            pass
+
+
+def main():
+    """ main
+    """
+    if len(sys.argv) != 2:
+        print("Need private host RSA key as argument.")
+        sys.exit(1)
+
+    host_key_path = sys.argv[1]
+    executor = concurrent.futures.ProcessPoolExecutor(SERVER_COUNT)
+    futures = [
+        executor.submit(_spawn, host_key_path, BASE_PORT + item)
+        for item in range(0, SERVER_COUNT)
+    ]
+    for future in concurrent.futures.as_completed(futures):
+        future.result()
+
+
+if __name__ == "__main__":
+    main()
