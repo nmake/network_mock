a1307c8bbf96f2fc0609359b2ed2195917b78fc2
Bradley A. Thornton <bthornto@thethorntons.net>
2019-08-18 08:02:17 -0700

    cleanup, interactive, help, history support

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ import re
 import concurrent.futures
 import paramiko
 import logging
+from os import listdir
+from os.path import isfile, join, splitext
 
-BASE_PORT = 3000
+BASE_PORT = 2200
 SERVER_COUNT = 10
 CONFIG_DIR = "./configs"
 
@@ -43,77 +45,175 @@ class Server(paramiko.ServerInterface):
         self.event.set()
         return True
 
+    def get_banner(self):
+        return ("", "")
 
-def run_server(transport):
-    channel = transport.accept(20)
-    if channel is None:
-        sys.exit(1)
 
-    prompt = "router#"
-    meta = {}
-    fhandle = channel.makefile("rU")
+class NetworkServer:
+    def __init__(self, host_key_path, port):
+        self._history = []
+        self._host_key_path = host_key_path
+        self._channel = None
+        self._conf_mode = False
+        self._port = port
+        self._session_meta = {}
+        self._transport = None
+        self._prompt = "router#"
 
-    channel.send("\r\n{}".format(prompt))
-    while True:
-        line = fhandle.readline().rstrip()
-        logger.info("%s: %s", meta.get("hostname", "unknown"), line)
+    def run(self):
+        host_key = paramiko.RSAKey(filename=self._host_key_path)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("", self._port))
+        logging.info("SSH server bound to port %s", self._port)
+        sock.listen(100)
+        client, addr = sock.accept()
+        logging.info("Connection from %s on port %s", addr, self._port)
+        self._transport = paramiko.Transport(client)
+        self._transport.load_server_moduli()
+        self._transport.add_server_key(host_key)
+        server = Server()
+        self._transport.start_server(server=server)
+        self._channel = self._transport.accept(20)
+        if self._channel is None:
+            sys.exit(1)
+        self._interactive()
+
+    def _is_conf(self, line):
+        confcmd = re.match(
+            r"(?P<conf>conf)(igure)?( (?P<conf_mode>.*))?", line
+        )
+        if confcmd:
+            cap = confcmd.groupdict()
+            logger.info(
+                "user entered configure mode with %s",
+                cap.get("conf_mode", "none"),
+            )
+            self._conf_mode = True
+
+    def _is_exit(self, line):
+        if line == "exit":
+            if self._conf_mode:
+                logger.info("user exited configure mode")
+                self._conf_mode = False
+            else:
+                self._transport.close()
+
+    def _is_help(self, line):
+        command = re.match(r"(\?|help)", line)
+        if command:
+            self._channel.send("\r\nGENERAL COMMANDS")
+            self._channel.send("\r\nexit                  Exit the session")
+            self._channel.send("\r\nhelp                  Get help")
+            self._channel.send("\r\nmeta: hostname=xxx    Set the hostname")
+            if "hostname" in self._session_meta:
+                hostdir = "{}/{}".format(
+                    CONFIG_DIR, self._session_meta["hostname"]
+                )
+                files = [
+                    splitext(f)[0]
+                    for f in listdir(hostdir)
+                    if isfile(join(hostdir, f))
+                ]
+                self._channel.send("\r\nAVAILABLE NETWORK COMMANDS")
+                self._channel.send("\r\n" + "\r\n".join(files))
+
+    def _is_history(self, line):
+        command = re.match(r"history", line)
+        if command:
+            formatted_history = [
+                "{}  {}".format(str(idx).rjust(3), cmd)
+                for idx, cmd in enumerate(self._history)
+            ]
+            self._channel.send("\r\n" + "\r\n".join(formatted_history))
+        command = re.match(r"!(?P<hnum>\d+)", line)
+        if command:
+            cap = command.groupdict()
+            try:
+                history_command = self._history[int(cap["hnum"])]
+                if not history_command.startswith("!"):
+                    self._channel.send("\r\n{}".format(history_command))
+                    self._handle_command(history_command)
+            except IndexError:
+                pass
+
+    def _is_meta(self, line):
         metacmd = re.match(r"meta: (?P<key>.*)=(?P<value>.*)", line)
         if metacmd:
             cap = metacmd.groupdict()
-            meta[cap["key"]] = cap["value"]
+            self._session_meta[cap["key"]] = cap["value"]
+            if cap["key"] == "hostname":
+                self._prompt = cap["value"] + "#"
+
+    def _is_show(self, line):
         showcmd = re.match(r"show (?P<value>.*)", line)
         if showcmd:
             cap = showcmd.groupdict()
-            if cap["value"] == "privilege" and "hostname" not in meta:
-                channel.send("\n15")
-            elif (
-                cap["value"] in ["version", "inventory"]
-                and "hostname" not in meta
-            ):
-                pass
-            elif "hostname" not in meta:
-                channel.send(
-                    '% hostname must be set with a "meta: hostname=xxxx" command'
-                )
-            else:
-                try:
-                    with open(
-                        "{}/{}/{}.txt".format(
-                            CONFIG_DIR, meta["hostname"], line
-                        ),
-                        "r",
-                    ) as fhand:
-                        content = fhand.read()
-                    channel.send(content)
-                except FileNotFoundError:
-                    channel.send(
-                        '% command file missing for "{}"'.format(line)
+            if "hostname" not in self._session_meta:
+                if cap["value"] == "privilege":
+                    self._channel.send("\n15")
+                elif cap["value"] in ["version", "inventory"]:
+                    pass
+                else:
+                    self._channel.send(
+                        "\r\n% hostname must be set with a "
+                        ' "meta: hostname=xxxx" command'
                     )
-        channel.send("\r\n{}".format(prompt))
+            else:
+                self._get_and_send(line)
 
+    def _get_and_send(self, line):
+        try:
+            with open(
+                "{}/{}/{}.txt".format(
+                    CONFIG_DIR, self._session_meta["hostname"], line
+                ),
+                "r",
+            ) as fhand:
+                content = fhand.read()
+            content = "\r\n".join(content.splitlines())
+            self._channel.send(content)
+        except FileNotFoundError:
+            self._channel.send('% command file missing for "{}"'.format(line))
 
-def listener(host_key_path, port):
-    host_key = paramiko.RSAKey(filename=host_key_path)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("", port))
-    sock.listen(100)
-    client, addr = sock.accept()
-    logging.info("connection from %s", addr)
-    transport = paramiko.Transport(client)
-    transport.load_server_moduli()
-    transport.add_server_key(host_key)
-    server = Server()
-    transport.start_server(server=server)
-    run_server(transport)
+    def _handle_command(self, line):
+        self._is_exit(line)
+        self._is_help(line)
+        self._is_history(line)
+        self._is_meta(line)
+        self._is_show(line)
+
+    def _interactive(self):
+        fhandle = self._channel.makefile("rU")
+        line_buffer = []
+        self._channel.send("\r\n{}".format(self._prompt))
+        while not self._channel.closed:
+            char = fhandle.read(1)
+            self._channel.send(char)
+            if char in [b"\x7f"]:  # backspace
+                self._channel.send("\b \b")
+                line_buffer = line_buffer[0:-1]
+            elif char not in [b"\r", b"\n"]:
+                line_buffer.append(char.decode("utf-8"))
+            else:
+                line = "".join(line_buffer)
+                self._history.append(line)
+                line_buffer = []
+                logger.info(
+                    "%s: %s",
+                    self._session_meta.get("hostname", "unknown"),
+                    line,
+                )
+                self._handle_command(line)
+                self._channel.send("\r\n{}".format(self._prompt))
 
 
 def _spawn(host_key_path, port):
     while True:
         try:
-            listener(host_key_path, port)
-        except Exception:  # pylint: disable=W0703
-            pass
+            NetworkServer(host_key_path, port).run()
+        except Exception as exc:  # pylint: disable=W0703
+            logger.warning(exc)
 
 
 def main():
